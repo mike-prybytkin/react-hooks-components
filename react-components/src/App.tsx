74bfc9fa5149d1a9dc68d9e208ab20118104e1ff
Mike Prybytkin <prybytkin@bk.ru>
2022-10-04 17:38:38 +0300

    feat: implement routing

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,15 +1,24 @@
 import AboutUs from 'pages/AboutUs/AboutUs';
 import Page404 from 'pages/404/404';
+import Main from 'pages/main/main';
+import NavBar from 'components/navbar/navBar';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/about" element={<AboutUs />} />
-      <Route path="/404" element={<Page404 />} />
-    </Routes>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/notFound" element={<Page404 />} />
+          <Route path="/*" element={<Navigate to="/notFound" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- react-components/src/pages/404/404.tsx
+++ react-components/src/pages/404/404.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
   return (
     <>
       <h1>Ошибка 404!</h1>
+      <Link to="/">На домашнюю страницу</Link>
     </>
   );
 };
