6aa3a5862e457a748b45e783071a4e3a9ea586fb
Mike Prybytkin <prybytkin@bk.ru>
2022-10-29 23:50:38 +0300

    feat: add user-card-list component

---
+++ react-components/src/components/user-card-list/user-card-list.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import UserCard from 'components/user-card/user-card';
+import { UserCardListProps } from './types';
+
+class UserCardList extends Component<UserCardListProps> {
+  renderCards() {
+    return this.props.cards.map((card) => (
+      <UserCard card={card} key={card.avatarPath.slice(0, 50)} />
+    ));
+  }
+  render() {
+    return <div className="user-cards-wrapper">{this.renderCards()}</div>;
+  }
+}
+
+export default UserCardList;
--- react-components/src/pages/form/form.tsx
+++ react-components/src/pages/form/form.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FormProps } from './types';
 import CreateUserForm from 'components/user-creating-form/user-creating-form';
+import UserCardList from 'components/user-card-list/user-card-list';
+import { mockUserCards } from 'mocks/user-cards';
 
 export default class Form extends Component<FormProps> {
   render() {
@@ -9,6 +11,7 @@ export default class Form extends Component<FormProps> {
         <h1 className="form-content-header">{this.props.heading}</h1>
         <div className="form-content-wrapper">
           <CreateUserForm />
+          <UserCardList cards={mockUserCards} />
         </div>
       </div>
     );
