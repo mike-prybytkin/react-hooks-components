164f124616edac74e1a3c834112e962543ec3944
Mike Prybytkin <prybytkin@bk.ru>
2022-11-06 01:15:53 +0300

    refactor: add type in LiveSearchProps

--- react-components/src/components/live-search/types.tsx
+++ react-components/src/components/live-search/types.tsx
@@ -1,11 +1,8 @@
-import { IProductCard } from 'share/types';
-
 export interface LiveSearchState {
   value: string;
 }
 
 export interface LiveSearchProps {
-  cards: IProductCard[];
   onSearch: (querySearch: string) => void;
   placeholderText: string;
 }
