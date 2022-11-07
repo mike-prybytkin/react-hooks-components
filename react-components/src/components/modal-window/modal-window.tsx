d2897eeaad69d4c6f41e60ad8637f800dc8df186
Mike Prybytkin <prybytkin@bk.ru>
2022-11-07 03:02:27 +0300

    feat: add tests to modal-window component

--- react-components/src/components/modal-window/modal-window.tsx
+++ react-components/src/components/modal-window/modal-window.tsx
@@ -3,10 +3,11 @@ import { ModalWindowProps } from './types';
 
 export default class ModalWindow extends Component<ModalWindowProps> {
   render() {
+    const { onModalWindow } = this.props;
     return (
-      <div className="modal-overlay" onClick={this.props.onModalWindow}>
+      <div className="modal-overlay" onClick={onModalWindow}>
         <div className="modal-window" onClick={(e) => e.stopPropagation()}>
-          <button className="modal-window__close" onClick={this.props.onModalWindow}>
+          <button className="modal-window__close" onClick={onModalWindow}>
             &#10007;
           </button>
           <div className="modal-window__body">{this.props.children}</div>
--- react-components/src/components/pagination/pagination.test.jsx
+++ react-components/src/components/pagination/pagination.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Pagination from './pagination';
 
 const mockCallback = jest.fn();
