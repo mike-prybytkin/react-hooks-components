fe5f34008714c0cd572fa32970e4377f9392f15a
Mike Prybytkin <prybytkin@bk.ru>
2022-11-09 22:04:07 +0300

    refactor: ModalWindow and NavBar components rewrite to functional components

--- react-components/src/components/nav-bar/nav-bar.tsx
+++ react-components/src/components/nav-bar/nav-bar.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar">
-        <ul className="navbar__items">
-          <li className="navbar__item">
-            <NavLink className="navbar__link" to="/" end>
-              Home
-            </NavLink>
-          </li>
-          <li className="navbar__item">
-            <NavLink className="navbar__link" to="/about">
-              About
-            </NavLink>
-          </li>
-          <li className="navbar__item">
-            <NavLink className="navbar__link" to="/form">
-              Form
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__items">
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/" end>
+            Home
+          </NavLink>
+        </li>
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/about">
+            About
+          </NavLink>
+        </li>
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/form">
+            Form
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
