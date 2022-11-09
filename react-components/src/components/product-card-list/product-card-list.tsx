9a076d52617b184c5d9409aed5bf11d3d81e502f
Mike Prybytkin <prybytkin@bk.ru>
2022-11-09 22:13:48 +0300

    refactor: ProductCardList and UserCardList rewrite to functional components

--- react-components/src/components/product-card-list/product-card-list.tsx
+++ react-components/src/components/product-card-list/product-card-list.tsx
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProductCard from 'components/product-card/product-card';
 import { CardListProps } from './types';
 import mockText from 'mocks/text';
 import Pagination from 'components/pagination/pagination';
 
-export class ProductCardList extends Component<CardListProps> {
-  constructor(props: CardListProps) {
-    super(props);
-  }
-
-  renderCards() {
-    return this.props.cards.map((card) => (
+const ProductCardList = (props: CardListProps) => {
+  const renderCards = () => {
+    return props.cards.map((card) => (
       <ProductCard buttonText={mockText.productCardButton} card={card} key={card.id} />
     ));
-  }
+  };
 
-  render() {
-    const isCards = this.props.cards.length >= 1;
-    return isCards ? (
-      <React.Fragment>
-        <div className="cards-wrapper">{this.renderCards()}</div>
-        <Pagination
-          queryPage={this.props.queryPage}
-          allPages={this.props.allPages}
-          cardsLimit={this.props.cardsLimit}
-          updateQuery={this.props.updateQuery}
-        />
-      </React.Fragment>
-    ) : (
-      <p className="not-found-items-message">{mockText.itemNotFound}</p>
-    );
-  }
-}
+  const hasCards = props.cards.length >= 1;
+  return hasCards ? (
+    <React.Fragment>
+      <div className="cards-wrapper">{renderCards()}</div>
+      <Pagination
+        queryPage={props.queryPage}
+        allPages={props.allPages}
+        cardsLimit={props.cardsLimit}
+        updateQuery={props.updateQuery}
+      />
+    </React.Fragment>
+  ) : (
+    <p className="not-found-items-message">{mockText.itemNotFound}</p>
+  );
+};
 
 export default ProductCardList;
--- react-components/src/components/user-card-list/user-card-list.tsx
+++ react-components/src/components/user-card-list/user-card-list.tsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserCard from 'components/user-card/user-card';
 import { UserCardListProps } from './types';
 
-class UserCardList extends Component<UserCardListProps> {
-  renderCards() {
-    return this.props.cards.map((card) => (
-      <UserCard card={card} key={card.avatarPath.slice(30, 70)} />
-    ));
-  }
-  render() {
-    return <div className="user-cards-wrapper">{this.renderCards()}</div>;
-  }
-}
+const UserCardList = (props: UserCardListProps) => {
+  const renderCards = () => {
+    return props.cards.map((card) => <UserCard card={card} key={card.avatarPath.slice(30, 70)} />);
+  };
+
+  return <div className="user-cards-wrapper">{renderCards()}</div>;
+};
 
 export default UserCardList;
